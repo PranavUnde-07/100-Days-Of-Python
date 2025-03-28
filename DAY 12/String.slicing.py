de22a8c8c0e584018ac12efe3bbf4ae5464907be
PranavUnde-07 <pranavunde45@gmail.com>
2025-03-28 17:05:21 +0530

    String slicing

--- DAY 12/String.slicing.py
+++ DAY 12/String.slicing.py
@@ -1,7 +1,25 @@
 # STRING SLICING
 
+
+
+# In this example, we will learn how to access characters in a string using string slicing.
+
 name = "Harry"
 print(name[-4:-1])
 
+# Output:
+
+# arr
+# In the above example, we have accessed the characters 'arr' from the string 'Harry' using string slicing.
+
+
+
+
+# In this example, we will learn how to access characters in a string using string slicing.
+
 fruit="banana"
 print(len(fruit))
+
+# Output:
+
+# 6
